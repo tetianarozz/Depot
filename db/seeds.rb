781d3183534a4b6e231a42dbb9d9ff61663e6545
tetianarozz <tatianarozputnia@gmail.com>
2022-02-22 15:26:58 +0200

    seed edit

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,3 +9,14 @@ Product.create!(title: 'Programming Ruby 1.9 & 2.0',
                     </p>},
                 image_url: 'ruby.jpg',
                 price: 49.95)
+
+Product.create!(title: 'CoffeeScript',
+                description:
+                  %{<p>
+                    CoffeeScript is a little language that compiles into JavaScript.
+                    Underneath that awkward Java-esque patina, JavaScript has always
+                    had a gorgeous heart. CoffeeScript is an attempt to expose the
+                    good parts of JavaScript in a simple way.
+                    </p>},
+                image_url: 'cs.jpg',
+                price: 20.55)
