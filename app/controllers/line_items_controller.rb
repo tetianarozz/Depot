aa3fda8c39a355ea15cacbb6789f1d407853c9cc
tetianarozz <tatianarozputnia@gmail.com>
2022-03-17 16:27:33 +0200

    format js for cart

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -28,7 +28,8 @@ class LineItemsController < ApplicationController
 
     respond_to do |format|
       if @line_item.save
-        format.html { redirect_to @cart }
+        format.html { redirect_to store_url }
+        format.js
         format.json { render :show, status: :created, location: @line_item }
       else
         format.html { render :new, status: :unprocessable_entity }
