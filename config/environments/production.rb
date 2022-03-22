eac13ff7761533a12dc52035bc3c33424d89909e
tetianarozz <tatianarozputnia@gmail.com>
2022-03-22 16:20:22 +0200

    Order Notifier Mailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,7 +1,20 @@
 require "active_support/core_ext/integer/time"
 
 Rails.application.configure do
-  # Settings specified here will take precedence over those in config/application.rb.
+  Rails.application.configure do
+    config.action_mailer.default_url_options = { host: ENV["PRODUCTION_HOSTNAME"] }
+    config.action_mailer.delivery_method = :smtp
+    config.action_mailer.smtp_settings = {
+      address: 'smtp.gmail.com',
+      port: 587,
+      domain: 'example.com',
+      user_name: ENV["SMTP_USER_NAME"],
+      password: ENV["SMTP_PASSWORD"],
+      authentication: 'plain',
+      enable_starttls_auto: true
+    }
+
+    # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
   config.cache_classes = true
