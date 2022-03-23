4f5d64e7e20cea992545b8910309d63b1a871449
tetianarozz <tatianarozputnia@gmail.com>
2022-03-23 16:43:14 +0200

    Order Notifier Mailer

--- app/mailers/order_notifier_mailer.rb
+++ app/mailers/order_notifier_mailer.rb
@@ -1,5 +1,5 @@
 class OrderNotifierMailer < ApplicationMailer
-  default from:'SamRuby<depot@example.com>'
+  default from:'Pragmatic Store<depot@example.com>'
   # Subject can be set in your I18n file at config/locales/en.yml
   # with the following lookup:
   #
