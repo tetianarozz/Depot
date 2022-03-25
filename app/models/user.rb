c6b9e391e8cdaf325dd51eb332323c7e2f8035d6
tetianarozz <tatianarozputnia@gmail.com>
2022-03-25 19:21:35 +0200

    administration

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,13 @@
 class User < ApplicationRecord
-  validates :name, presence: true, uniqueness: true
+  after_destroy :ensure_an_admin_remains
   has_secure_password
+  validates :name, presence: true, uniqueness: true
+
+  private
+
+  def ensure_an_admin_remains
+    if User.count.zero?
+      raise "Can't delete last user"
+    end
+  end
 end
