2612d93321d57eea04ad1c2db3824f02924fe947
tetianarozz <tatianarozputnia@gmail.com>
2022-03-28 12:14:08 +0300

    annotate models

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,13 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id              :integer          not null, primary key
+#  name            :string
+#  password_digest :string
+#  created_at      :datetime         not null
+#  updated_at      :datetime         not null
+#
 class User < ApplicationRecord
   after_destroy :ensure_an_admin_remains
   has_secure_password
